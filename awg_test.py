8fcd56680fa074a398dcc8b62b8d4e36f8e5b8b9
Reed Foster <reed23313@gmail.com>
2022-07-27 14:26:41 -0400

    figured out delay stuff

--- awg_test.py
+++ awg_test.py
@@ -4,16 +4,24 @@
 import sys
 import keysightSD1
 
-waveform_data_list = [i/100 for i in range(100)]
-
 # MODULE CONSTANTS
 PRODUCT = "M3202A"
 CHASSIS = 1
 
 # change slot number to your value
 SLOT = 5
-CHANNEL = 1
+CHANNELS = [1, 2, 3]
+DELAYS = [0, 0, 0]
 AMPLITUDE = 1.0
+
+#waveform_data_list = [i/100 for i in range(100)]
+waveform_data_list = [[0]*100 for channel in CHANNELS]
+for channel in CHANNELS:
+    if channel == 1 or channel == 2:
+        waveform_data_list[channel-1] = [0, 0, 0.5, 1, 0.7, 0.2] + [0]*4
+    else:
+        waveform_data_list[channel-1] = [0.5, 1, 0.7, 0.2, 0] + [0]*5
+    waveform_data_list[channel-1] = waveform_data_list[channel-1] * 5
 
 # CREATE AND OPEN MODULE
 module = keysightSD1.SD_AOU()
@@ -26,40 +34,32 @@ else:
     print("slot:", module.getSlot())
     print("Chassis:", module.getChassis())
     print()
-    module.channelWaveShape(CHANNEL, keysightSD1.SD_Waveshapes.AOU_AWG)
-    module.channelAmplitude(CHANNEL, AMPLITUDE)
-
-    # WAVEFORM FROM FILE
-    WAVE_NUMBER = 0
-
-    # create, open from file, load to module RAM and queue for execution 
     wave = keysightSD1.SD_Wave()
+    for channel in CHANNELS:
+        module.channelWaveShape(channel, keysightSD1.SD_Waveshapes.AOU_AWG)
+        module.channelAmplitude(channel, AMPLITUDE)
 
-    # set path to your file here
-    wave.newFromFile("C:/Users/Public/Documents/keysightSD1/Examples/Waveforms/Gaussian.csv")
-    module.waveformLoad(wave, WAVE_NUMBER)
-    module.AWGqueueWaveform(CHANNEL, WAVE_NUMBER, 0, 0, 0, 0)
-    error = module.AWGstart(CHANNEL)
-    if error < 0:
-        print("AWG from file error:", error)
-    else:
-        print("AWG from file started successfully")
-    input("Press any key to stop AWG")
-    module.AWGflush(CHANNEL)
-    module.AWGstop(CHANNEL)
-    input("Press any key to start AWG from array")
-
-    # WAVEFORM FROM ARRAY/LIST
-    # This function is equivalent to create a waveform with new,
-    # and then to call waveformLoad, AWGqueueWaveform and AWGstart
-    error = module.AWGfromArray(CHANNEL, 0, 0, 0, 0, 0, waveform_data_list)
-    if error < 0:
-        print("AWG from array error:", error)
-    else:
-        print("AWG from array started successfully")
+        # WAVEFORM FROM ARRAY/LIST
+        # This function is equivalent to create a waveform with new,
+        # and then to call waveformLoad, AWGqueueWaveform and AWGstart
+        # AWGfromArray(channel, triggerMode, startDelay, cycles, prescaler, waveformType, waveformDataA)
+        # channel - AWG channel number (e.g. 1-4)
+        # triggerMode - 0: AUTOTRIG (immediate, upon AWGStart or when previous waveform in the queue finishes)
+        #               1: SWHVITRIG (software trigger, upon AWGtrigger) 
+        #               5: SWHVITRIG_CYCLE (identical to SWHVITRIG, except a trigger is required to each cycle of the waveform)
+        #               2: EXTTRIG (hardware trigger, set by externalSource in AWGtriggerExternalConfig)
+        #               6: ExTTRIG_CYCLE
+        # startDelay - delay in multiples of 5ns for M3202A, multiples of 10ns for M3201A, max values 2^16 - 1
+        #               to get finer resolution, change the delay manually by changing the data stored in the buffer
+        error = module.AWGfromArray(channel, 0, DELAYS[channel-1], 0, 0, 0, waveform_data_list[channel-1])
+        if error < 0:
+            print(f"AWG from array error (channel {channel}):", error)
+        else:
+            print("AWG from array started successfully")
     # exiting...
     input("Press any key to stop AWG")
-    module.AWGstop(CHANNEL)
+    for channel in CHANNELS:
+        module.AWGstop(channel)
     module.close()
     print()
     print("AOU closed")
